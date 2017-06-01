4fd73c336003ccd4ec3b34879bb7d8a9ad1633b5
Peter Malcolm <peter.t.malcolm@gmail.com>
2017-06-01 12:21:05 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// use graceful-fs just like fs 
+const fs = require('graceful-fs');
+const lessonsFolder = '../governance-content/';
+const re = /^\d+_\d/;
+var lessonsList = [];
+const urlPrefix = 'http://governance-staging.netlify.com/';
+
+// grab the dirs in /governance-content/
+fs.readdirSync(lessonsFolder).forEach(file => {
+	if(file.search(re) !== -1) {
+		lessonsList.push(file);
+	}
+});
+
+// sort them
+lessonsList = lessonsList.sort(function compare(a,b){
+	var beginDiff = valBegin(a) - valBegin(b);
+	if( beginDiff !== 0 ) {
+		return beginDiff;
+	} else {
+		return valMid(a) - valMid(b);
+	}
+});
+
+// output them:
+fs.writeFile("index.html", content(lessonsList, urlPrefix), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
+
+/////// HELPERS:
+function valBegin(str) {
+	return parseInt(str.slice(0,str.indexOf('_')), 10);
+}
+function valMid(str) {
+	var re2 = /_(\d+)_/;
+	return parseInt(str.match(re2)[1],10);
+}
+function content(list,prefix) {
+	var html = '<!DOCTYPE html><html><head><style>ul{list-style:none;height:80px;overflow:scroll;position:-webkit-sticky;position:sticky;top:0;}iframe{position:fixed;width:100%;height:600px;top:100px;}</style></head><body><ul>';
+	for (var i = 0; i < list.length; i++) {
+		html += '<li><a href="'+prefix+list[i]+'" target="f">'+list[i]+'</a></li>';
+	}
+	html += '</ul><iframe name="f"></iframe></body></html>';
+	return html;
+}
